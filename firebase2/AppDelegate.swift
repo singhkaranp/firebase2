19334972d7b5a6ebf6f1eb82ef95156b42b426ba
singhkaranp <karanpal13@gmail.com>
2019-11-21 19:16:58 -0600

    added a comment in appdelegate

--- firebase2/AppDelegate.swift
+++ firebase2/AppDelegate.swift
@@ -11,7 +11,7 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    //add comment
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
