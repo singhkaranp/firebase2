2d416fd0dc2a1c0506c32e5582ade066ee697235
singhkaranp <karanpal13@gmail.com>
2019-11-21 19:02:05 -0600

    Initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'firebase2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for firebase2
+	pod 'Firebase/Analytics'
+	pod 'SwiftyJSON'
+
+end
---
+++ firebase2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase2
+//
+//  Created by Karan Singh on 11/21/19.
+//  Copyright © 2019 Karan Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
